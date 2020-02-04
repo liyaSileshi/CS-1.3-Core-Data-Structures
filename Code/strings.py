c7a9b82c89d5a2fe77de916e5f6b98a9059e29f9
liyaSileshi <liyasileshi@gmail.com>
2020-02-03 23:16:58 -0800

    working on strings.py

--- Code/strings.py
+++ Code/strings.py
@@ -5,7 +5,34 @@ def contains(text, pattern):
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
     # TODO: Implement contains here (iteratively and/or recursively)
-
+    # num = 0
+    # while num < len(pattern):
+    #     for i in range(len(text)):
+    #         # while num < len(pattern):
+    #         if text[i] == pattern[num]: #pattern is being found
+    #                 # print(i, text[i])
+    #             num += 1
+                # print('num',num)
+                # print('pattern', len(pattern))
+    # if num < len(pattern): #pattern not found
+    #     return False
+    # elif num == len(pattern): #pattern found
+    #     return True
+    # else:
+    #     return False
+    num = 0
+    for i in range(len(pattern)):
+        for j in range(len(text)):
+            if pattern[i] == text[j]:
+                print(pattern[i])
+                num += 1
+                break
+    # print(num)
+    if len(pattern) == num:
+        return True
+    else:
+        return False
+            
 
 def find_index(text, pattern):
     """Return the starting index of the first occurrence of pattern in text,
@@ -53,4 +80,5 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    # main()
+    print(contains('aaa','aa'))
