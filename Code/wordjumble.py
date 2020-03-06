b6bc51af93c23c4920cfd63f1b43f5014c51f024
liyaSileshi <liyasileshi@gmail.com>
2020-03-06 11:07:05 -0800

    it works

--- Code/wordjumble.py
+++ Code/wordjumble.py
@@ -1,7 +1,5 @@
-import timeit
-
-
 import time
+
 def timer(func):
     def _timer(*args):
         start_time = time.time()
@@ -13,23 +11,27 @@ def timer(func):
 
 @timer
 def unjumble(jumble_word):
-    word_list = []
-    new_list = dict()
+    word_list = [] #will have the list of all the dictionary words
+    new_list = dict() #create a dictionary
     with open("/usr/share/dict/words", "r") as file:
         all_word = file.read().lower()
         words = all_word.split('\n')
-        for w in words:
-            word_list.append(w)
+        for word in words:
+            word_list.append(word)
 
-    for word in jumble_word:
-        possible_words = []
-        for w in word_list:
-            if len(w) == len(word) and sorted(w) == sorted(word): #if the word in the dictionary have same length as jumble word
-                possible_words.append(w)
+    for word in jumble_word: #looping over the jumble_word list
+        possible_words = [] 
+        for dict_word in word_list: 
+            #if the word in the dictionary have same length as jumble word
+            if len(dict_word) == len(word) and sorted(dict_word) == sorted(word.lower()): 
+                possible_words.append(dict_word)
             new_list[word] = possible_words #dictionary to new list
-
     print(new_list)
-    return word_list
+    return new_list
 
 if __name__ == '__main__':
-    unjumble(['laurr', 'laisa', 'bureek','prouot'])
+    word1 = ['TEFON', 'SOKIK', 'NIUMEM', 'SICONU']
+    pick1 = [[2,4], [0,1,3], [4], [3,4]]
+    word2 = ['laurr', 'laisa', 'bureek','prouot']
+    unjumble(word2)
+    unjumble(word1)
